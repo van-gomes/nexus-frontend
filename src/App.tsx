51c1fdb9263021a862c2c93a68a5ac10810211ce
van-gomes <nninha.gomes@gmail.com>
2024-07-30 22:08:18 -0300

    Using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { Layout } from "./components/Layout";
 import { Login } from "./components/Login";
+import styled from "styled-components";
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+  background-color: blue;
+`;
 
 function App() {
   return (
     <Layout>
+       <Title>
+        Styled Components
+      </Title>
       <Login />
     </Layout>
   );
