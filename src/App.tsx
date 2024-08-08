826821aa83a0ad19e0148512c691ea600ecc513f
van-gomes <nninha.gomes@gmail.com>
2024-08-07 21:09:19 -0300

    Use Context API

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,39 @@ import { Conta } from "./pages/Conta";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "./components/Layout";
 import { DetalhesConta } from "./pages/DetalhesConta";
+import { createContext } from "react";
+
+interface IAppContext {
+  user: string
+}
+
+export const AppContext = createContext({} as IAppContext);
+
+const AppContextProvider = ({ children }: any) => {
+  const user = 'Vânia';
+
+  return (
+    <AppContext.Provider value={{ user }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <ChakraProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/conta/:id" element={<Conta />} />
-            <Route path="/detalhesconta" element={<DetalhesConta />} />
-          </Routes>
-        </Layout>
-      </ChakraProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AppContextProvider>
+          <ChakraProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/conta/:id" element={<Conta />} />
+                <Route path="/detalhesconta" element={<DetalhesConta />} />
+              </Routes>
+            </Layout>
+          </ChakraProvider>
+        </AppContextProvider>
+      </BrowserRouter>
   );
 }
 
